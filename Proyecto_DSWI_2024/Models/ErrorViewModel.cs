428115ecc67b4f496a1568e7502c751815f2c17a
joshuapereda <joshuadesihuas@hotmail.com>
2024-04-25 17:42:06 -0500

    PRIMER COMMIT

---
+++ Proyecto_DSWI_2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_DSWI_2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
